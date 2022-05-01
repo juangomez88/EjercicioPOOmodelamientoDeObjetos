cb03dc4ff438ae9b5d75363f7cb585259275a627
juangomez88 <60585685+juangomez88@users.noreply.github.com>
2022-05-01 09:12:10 -0500

    Punto 2 taller 4
    Creo la rama del punto 2 del taller # 4

---
+++ src/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public static void main(String[] args) {
+        MyClass test = new MyClass();
+        test.suma();
+
+        }
+        public void suma(){
+            System.out.println("Es una suma: 2 + 3 = " + (2+3));
+        }
+
+
+}
